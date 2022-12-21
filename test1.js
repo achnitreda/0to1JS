c4fb96231a822ed62da42b5fa31135de3fc93144
achnitreda <achnitreda@gmail.com>
2022-12-21 22:28:44 +0100

    2-create a fn that contains 2 arguments first it's array of words that we should filter and seconf arg it's a string of letters used to filter our words return an array of filtered words

--- test1.js
+++ test1.js
@@ -29,4 +29,26 @@ console.log(countWords(words));
 // Time Complexity of the function: O(n+n) = O(n)
 
 
+// 2- create a fn that contains 2 arguments first it's array of words that we should filter and seconf arg it's a string of letters used to filter our words return an array of filtered words
 
+function filterWords(words,letters){
+    
+    const letterArray = letters.split('');
+    console.log(letterArray);
+ 
+    const filteredWords = words.filter(word => {
+     const wordLetters = word.split('');
+     console.log(wordLetters);
+ 
+     return letterArray.every(letter => wordLetters.includes(letter)); /* Checking if every letter in the letterArray is included in the wordLetters array. */
+    })
+    
+    return filteredWords;
+ }
+ 
+ const words = ['apple','banana','orange','mango','grapes']
+ const letters = 'p';
+ 
+ console.log(filterWords(words,letters)) // ['apple','grapes']
+
+//  complexity O(n*m) ??
