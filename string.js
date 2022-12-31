6687d90a5365bf7f243e8657c31d2f7c6f6759f9
achnitreda <achnitreda@gmail.com>
2022-12-31 15:02:41 +0100

    solve Is Subsequence problem using two pointers solution

--- string.js
+++ string.js
@@ -1,5 +1,9 @@
-const s = "abc"
-const t = "ahbgdc"
+/**
+ * 4h+days
+ * take a course
+ */
+const s = "abc".split('')
+const t = "ahbgdc".split('');
 
 /**
  * @param {string} s
@@ -7,23 +11,19 @@ const t = "ahbgdc"
  * @return {boolean}
  */
 var isSubsequence = function(s, t) {
-    for(let i=0; i< s.length; i++){
-        console.log(s[i]);
-
-        for(let j=0; j< t.length; j++){
-            console.log(t[j]);
-            if(s[i] == t[j]){
-                i++;
-            }else{
-                j++;
-            }
-
-            if(i == s.length){
-                return true;
-            }else{
-                return false
-            }
+    let p1=0,p2 =0;
+    while(p1< s.length && p2< t.length){
+        if(s[p1] != t[p2]){
+            p2++;
+        }else{
+            p1++;
+            p2++;
         }
+    }
+    if(p1 === s.length){
+        return true;
+    }else{
+        return false;
     }
 };
 
