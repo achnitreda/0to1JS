a7565e30860e33ffd3541ed962e5e2408b2e922e
achnitreda <achnitreda@gmail.com>
2023-01-01 21:49:08 +0100

    greater number in an array

--- algo.js
+++ algo.js
@@ -1,1 +1,13 @@
+// greater number in an array
+
+let array = [12,23,45,2,1,55,87.7,43,76,69,70]
+let max = 0; 
+for(let i=0; i<array.length; i++){
+    if(max < array[i]){
+        max = array[i]
+    }
+}
+console.log(max) // 76
+
+//  43:06
 
